0ff1a755ca4496e5729e5193ac80006de770b5f3
Amgad Malik <amgadmalik@yahoo.com>
2019-10-21 00:34:42 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,7 +6,7 @@ doc = Nokogiri::HTML(open("https://flatironschool.com/"))
 header = doc.css(".headline-26OIBN").text
 puts header
 courses = doc.css("div.gridContainer-EnvccM")
-binding.pry
+#binding.pry
 courses.each do |course|
   puts course.text.strip
 end
