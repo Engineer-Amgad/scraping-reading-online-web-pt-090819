b0dadc9320f4c76e46d240912dd115e5cd03d020
Amgad Malik <amgadmalik@yahoo.com>
2019-10-21 00:29:26 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,5 +4,5 @@ require 'open-uri'
 doc = Nokogiri::HTML(open("https://flatironschool.com/"))
 header = doc.css(".headline-26OIBN").text
 puts header
-cources = doc.css(gridContainer-En)
+cources = doc.css(gridContainer-EnvccM)
 #\32 a778efd-1685-5ec6-9e5a-0843d6a88b7b > div > div.content-3OTK_R > div
