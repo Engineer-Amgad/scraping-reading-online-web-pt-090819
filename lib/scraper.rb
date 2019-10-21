1851522487494110d1bd3025e850426b4c141b52
Amgad Malik <amgadmalik@yahoo.com>
2019-10-21 00:27:37 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,3 +5,4 @@ doc = Nokogiri::HTML(open("https://flatironschool.com/"))
 header = doc.css(".headline-26OIBN").text
 puts header
 
+#\32 a778efd-1685-5ec6-9e5a-0843d6a88b7b > div > div.content-3OTK_R > div
